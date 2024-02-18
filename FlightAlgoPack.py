33cb7ef5a55641bfd2eed5d298e8df52d509a687
RageFu2004 <115386399+RageFu2004@users.noreply.github.com>
2024-02-18 16:50:46 -0600

    Update FlightAlgoPack.py
    add rating details telling you the severeness of given flight

--- FlightAlgoPack.py
+++ FlightAlgoPack.py
@@ -94,6 +94,7 @@ class algopack:
         #print(time1)
         modified_map = self.data_modify(turbo_array)
         turboScore = 0
+        totol_length = 0
         for index in range(len(flight_path)-1):
             if flight_path[index][0]>flight_path[index+1][0]:
                 start = flight_path[index+1]
@@ -106,14 +107,53 @@ class algopack:
             inclination = (end_y-start_y)/(end_x-start_x)
             begin_y = start_y+inclination*(math.ceil(start_x)-start_x)
             turboScore += math.sqrt((begin_y-start_y)**2+(math.ceil(start_x)-start_x)**2)*modified_map[math.floor(start_y)][math.floor(start_x)]
+            totol_length += math.sqrt((begin_y-start_y)**2+(math.ceil(start_x)-start_x)**2)
             for partition in range(math.ceil(start_x),math.floor(end_x)):
                 begin_y += inclination
                 turboScore += math.sqrt(inclination**2+1)*modified_map[math.ceil(begin_y)][partition]
             turboScore+=math.sqrt(((end_x-math.floor(end_x))**2+(end_y-begin_y)**2))*modified_map[math.floor(end_y)][math.floor(end_x)]
+            totol_length += math.sqrt((begin_y-start_y)**2+(math.ceil(start_x)-start_x)**2)
         #time2 = time.time()
         #print(time2)
-        return turboScore
+        return (turboScore/totol_length)*100
 
+    '''
+    return a string telling you the severeness of this flight
+    param: flight_path, turbo_array, plane_weight(in lbs)
+    '''
+    def turboRate(self, flight_path, turbo_array, plane_weight):
+        turboScore = self.turboCalc(flight_path, turbo_array)
+
+        if plane_weight <= 15500:
+            if turboScore <= 16:
+                return "Light"
+            if 16 < turboScore <= 36:
+                return "Moderate"
+            if 36 < turboScore <= 64:
+                return "Severe"
+            if turboScore > 64:
+                return "Extreme"
+        elif 15500 < plane_weight <= 300000:
+            if turboScore <= 20:
+                return "Light"
+            if 20 < turboScore <= 44:
+                return "Moderate"
+            if 44 < turboScore <= 79:
+                return "Severe"
+            if turboScore > 79:
+                return "Extreme"
+        elif plane_weight > 300000:
+            if turboScore <= 24:
+                return "Light"
+            if 24 < turboScore <= 54:
+                return "Moderate"
+            if 54 < turboScore <= 96:
+                return "Severe"
+            if turboScore > 96:
+                return "Extreme"
+
+
+# test_data
 if __name__ == "__main__":
     calc = algopack([1400,700])
     lst = []
@@ -125,6 +165,11 @@ if __name__ == "__main__":
     flight_path = [(36.1245, -86.6782), (35.9977, -85.0491), (35.9048, -83.8947), (35.1847, -79.9919),
                    (37.5335, -75.8581), (38.6486, -75.0885), (39.0955, -74.8003), (40.6401, -73.7765)]
     print(calc.turboCalc(flight_path, lst))
+
+
+
+
+
 
 
 
